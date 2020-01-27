33cf023b2af1e607e36c13f0734a8bc8cc1a8870
Karol Lobos <karollobos@interia.pl>
2020-01-28 00:15:14 +0100

    Edit src of contact image

--- components/Contact.js
+++ components/Contact.js
@@ -6,7 +6,7 @@ var Contact = React.createClass({
     render: function() {
         return (
             <div className={'contactItem'}>
-                <img className={'contactImage'} src={'link-do-obrazka.png'} />
+                <img className={'contactImage'} src={'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTEwHtI3OSsxW6E26sKmxWOqa80RAiWfvJDk-IKS8ZxznhcVHfi'} />
                 <p className={'contactLabel'}>
                     Imię: {this.props.item.firstName}
                 </p>
